9a73a17ab020af2b0a751c938c15586604615be4
khanhtbh <khanhtbh@fpt.com>
2024-07-06 11:47:36 +0700

    Add Movie Detail screen

--- src/network/API.ts
+++ src/network/API.ts
@@ -1,80 +1,50 @@
 import axios from 'axios';
-import {Movie} from '../models/Movie';
+import {Movie, MovieDetail} from '../models/Movie';
 
 const API_BASE_URL = 'https://search.imdbot.workers.dev/';
 
-export const getMovies = async (): Promise<Movie[]> => {
-  try {
-    const randomKeyword = [
-      'movies',
-      'prime video',
-      'labor day',
-      'godzilla minus one',
-      'united states',
-      'a.i.',
-      'robert de niro',
-      'austin butler',
-      'eddie murphy',
-      'inside out 2',
-      'star wars',
-      'tom hardy',
-      'horror movie',
-      'horror movies',
-      'jack nicholson',
-      'kirsten dunst',
-      'tv shows',
-      'freddie kreuger',
-      'late night with the devil',
-      'michael myers',
-      'monkey man',
-      'russell crowerussell',
-      'the fall guy',
-    ];
-    const keyword = randomKeyword.at(
-      (Math.random() * 100) % randomKeyword.length,
-    );
-    console.log('initiate keyword: ' + keyword);
-    const response = await axios.get(`${API_BASE_URL}?q=${keyword}`);
-    const data = response.data.description;
-    var movies: Movie[] = [];
-    for (const item of data) {
-      var movie = parseMovie(item);
-      movies.push(movie);
+class API {
+  static async searchMovies(query: string): Promise<Movie[]> {
+    try {
+      const response = await axios.get(`${API_BASE_URL}?q=${query}`);
+      const data = response.data.description;
+      var movies: Movie[] = [];
+      for (const item of data) {
+        var movie = API.parseMovie(item);
+        movies.push(movie);
+      }
+      return movies;
+    } catch (error) {
+      console.error('Error searching movies:', error);
+      throw error;
     }
-    return movies;
-  } catch (error) {
-    console.error('Error fetching movies:', error);
-    throw error;
   }
-};
 
-export const searchMovies = async (query: string): Promise<Movie[]> => {
-  try {
-    const response = await axios.get(`${API_BASE_URL}?q=${query}`);
-    const data = response.data.description;
-    var movies: Movie[] = [];
-    for (const item of data) {
-      var movie = parseMovie(item);
-      movies.push(movie);
+  static async getMovie(id: string): Promise<MovieDetail> {
+    try {
+      const response = await axios.get(`${API_BASE_URL}?tt=${id}`);
+      const movie: MovieDetail = response.data.short;
+      return movie;
+    } catch (error) {
+      console.error('Error getting movie:', error);
+      throw error;
     }
-    return movies; // Cast the data to Movie[]
-  } catch (error) {
-    console.error('Error searching movies:', error);
-    throw error;
   }
-};
 
-function parseMovie(data: any): Movie {
-  return {
-    title: data['#TITLE'],
-    year: data['#YEAR'],
-    id: data['#IMDB_ID'],
-    rank: data['#RANK'],
-    aka: data['#AKA'],
-    imdbUrl: data['#IMDB_URL'],
-    imdbIv: data['#IMDB_IV'],
-    imgPoster: data['#IMG_POSTER'],
-    photoWidth: data['photo_width'],
-    photoHeight: data['photo_height'],
-  };
+  private static parseMovie(data: any): Movie {
+    return {
+      title: data['#TITLE'],
+      year: data['#YEAR'],
+      id: data['#IMDB_ID'],
+      rank: data['#RANK'],
+      aka: data['#AKA'],
+      imdbUrl: data['#IMDB_URL'],
+      imdbIv: data['#IMDB_IV'],
+      imgPoster: data['#IMG_POSTER'],
+      photoWidth: data.photo_width,
+      photoHeight: data.photo_height,
+    };
+  }
 }
+
+export default API;
--- src/stores/MovieStore.ts
+++ src/stores/MovieStore.ts
@@ -1,10 +1,38 @@
 import {makeAutoObservable, runInAction} from 'mobx';
-import {getMovies, searchMovies} from '../network/API';
-import {Movie} from '../models/Movie';
+import API from '../network/API';
+import {Movie, MovieDetail} from '../models/Movie';
+
+const randomKeywords = [
+  'movies',
+  'prime video',
+  'labor day',
+  'godzilla minus one',
+  'united states',
+  'a.i.',
+  'robert de niro',
+  'austin butler',
+  'eddie murphy',
+  'inside out 2',
+  'star wars',
+  'tom hardy',
+  'horror movie',
+  'horror movies',
+  'jack nicholson',
+  'kirsten dunst',
+  'tv shows',
+  'freddie kreuger',
+  'late night with the devil',
+  'michael myers',
+  'monkey man',
+  'russell crowe',
+  'russell',
+  'the fall guy',
+];
 
 class MovieStore {
   movies: Movie[] = [];
   searchResults: Movie[] = [];
+  movieDetail: MovieDetail | null = null;
   isLoading: boolean = false;
   error: string | null = null;
 
@@ -13,31 +41,52 @@ class MovieStore {
   }
 
   async fetchMovies() {
-    this.isLoading = true;
-    this.error = null;
+    runInAction(() => {
+      this.isLoading = true;
+      this.error = null;
+    });
     try {
-      const data = await getMovies();
+      const keyword: string =
+        randomKeywords.at((Math.random() * 100) % randomKeywords.length) ?? '';
+      const data = await API.searchMovies(keyword);
       runInAction(() => {
         this.movies = data.slice(0, Math.min(10, data.length));
       });
     } catch (error) {
-      this.error = 'Failed to fetch movies';
+      runInAction(() => (this.error = 'Failed to fetch movies'));
     } finally {
-      this.isLoading = false;
+      runInAction(() => (this.isLoading = false));
     }
   }
 
   async search(query: string) {
-    this.isLoading = true;
-    this.error = null;
+    runInAction(() => {
+      this.isLoading = true;
+      this.error = null;
+    });
     try {
-      const data = await searchMovies(query);
-      console.log('Search Data ', data);
+      const data = await API.searchMovies(query);
       runInAction(() => (this.searchResults = data));
     } catch (error) {
-      this.error = 'Failed to search movies';
+      runInAction(() => (this.error = 'Failed to search movies'));
     } finally {
-      this.isLoading = false;
+      runInAction(() => (this.isLoading = false));
+    }
+  }
+
+  async getMovieDetail(id: string) {
+    runInAction(() => {
+      this.isLoading = true;
+      this.error = null;
+    });
+
+    try {
+      const data = await API.getMovie(id);
+      runInAction(() => (this.movieDetail = data));
+    } catch (error) {
+      runInAction(() => (this.error = 'Failed to get movie detail'));
+    } finally {
+      runInAction(() => (this.isLoading = false));
     }
   }
 }
